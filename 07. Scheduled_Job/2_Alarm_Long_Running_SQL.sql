176f23fbf962282f74e6be00b7fde98a3b12bbcd
Mostafa Chegenizadeh <mostafachegeni@gmail.com>
2021-09-12 00:19:11 +0430

    Update 2_Alarm_Long_Running_SQL.sql

--- 07. Scheduled_Job/2_Alarm_Long_Running_SQL.sql
+++ 07. Scheduled_Job/2_Alarm_Long_Running_SQL.sql
@@ -2,7 +2,7 @@
 [ ]$ mkdir -p /export/home/mohaymendba/script/log/
 
 ------------------------------------------------------------------------------------------------
-[ ]$ cat /export/home/mohaymendba/script/long_running_trans_invdb4_find.sql
+[ ]$ cat /export/home/mohaymendba/script/long_running_trans_db_find.sql
 set line 300
 col username for a15
 col machine for a30
@@ -11,7 +11,7 @@ set feed off
 --set head off
 alter session set nls_calendar=gregorian;
 alter session set nls_date_format='DD-MON-YY HH24:MI:SS';
-spool /export/home/mohaymendba/script/long_running_trans_invdb4.sql
+spool /export/home/mohaymendba/script/long_running_trans_db.sql
 --set time on
 --set timing on
 SELECT 	username, 
@@ -29,40 +29,37 @@ spool off
 exit;
 
 -----------------------------------------------------------------------------------------------
-[oracle@moschdb ~]$ cat /export/home/mohaymendba/script/long_running_invdb4_alert.ksh
+[oracle@moschdb ~]$ cat /export/home/mohaymendba/script/long_running_db_alert.ksh
 
 #!/bin/bash
 
-export ORACLE_SID=invdb4
+export ORACLE_SID=db
 export ORACLE_HOME=/oracle/product/19c/db_1
 export LD_LIBRARY_PATH=/oracle/product/19c/db_1/lib:/lib
 export PATH=$ORACLE_HOME/bin:$PATH
 
 
 #Empty File Content by Redirecting to Null:
-> /export/home/mohaymendba/script/long_running_trans_invdb4.sql
+> /export/home/mohaymendba/script/long_running_trans_db.sql
 
 #Run SQL Files:
-sqlplus INVENTIVE/InV19_Ts#400 << EOF
-@/export/home/mohaymendba/script/long_running_trans_invdb4_find.sql
+sqlplus USERNAME/PASSWORD << EOF
+@/export/home/mohaymendba/script/long_running_trans_db_find.sql
 EOF
 
 #Send an Email if the file is not empty:
-if [[ -s /export/home/mohaymendba/script/long_running_trans_invdb4.sql ]]
+if [[ -s /export/home/mohaymendba/script/long_running_trans_db.sql ]]
 then
-cat /export/home/mohaymendba/script/long_running_trans_invdb4.sql | mailx -s "INVDB4_ALERT_LONG_RUNNING" ext.h.esfandiari@mci.ir
-cat /export/home/mohaymendba/script/long_running_trans_invdb4.sql | mailx -s "INVDB4_ALERT_LONG_RUNNING" ext.m.chegenizadeh@mci.ir
-cat /export/home/mohaymendba/script/long_running_trans_invdb4.sql | mailx -s "INVDB4_ALERT_LONG_RUNNING" s.rostampour@mci.ir
-cat /export/home/mohaymendba/script/long_running_trans_invdb4.sql | mailx -s "INVDB4_ALERT_LONG_RUNNING" a.oliyaei@mci.ir
+cat /export/home/mohaymendba/script/long_running_trans_db.sql | mailx -s "db_ALERT_LONG_RUNNING" email@email.ir
 fi
 
 #Empty File Content by Redirecting to Null:
-> /export/home/mohaymendba/script/long_running_trans_invdb4.sql
+> /export/home/mohaymendba/script/long_running_trans_db.sql
 
 -----------------------------------------------------------------------------------------------
-[oracle@moschdb ~]$ chmod +x /export/home/mohaymendba/script/long_running_invdb4_alert.ksh
+[oracle@moschdb ~]$ chmod +x /export/home/mohaymendba/script/long_running_db_alert.ksh
 
 [oracle@moschdb ~]$ crontab -e
-00,30 * * * * /export/home/mohaymendba/script/long_running_invdb4_alert.ksh >> /export/home/mohaymendba/script/log/long_running_invdb4_alert.log
+00,30 * * * * /export/home/mohaymendba/script/long_running_db_alert.ksh >> /export/home/mohaymendba/script/log/long_running_db_alert.log
 
 [oracle@moschdb ~]$ crontab -l
