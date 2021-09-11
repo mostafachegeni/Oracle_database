2fd995b0d28407606fb650b5ebe4d71937f383c4
Mostafa Chegenizadeh <mostafachegeni@gmail.com>
2021-09-12 00:15:54 +0430

    Update 1_Alarm_Locks.sh

--- 07. Scheduled_Job/1_Alarm_Locks.sh
+++ 07. Scheduled_Job/1_Alarm_Locks.sh
@@ -1,8 +1,8 @@
 ------------------------------------------------------------------------------------------------
-[ ]$ mkdir -p /export/home/mohaymendba/script/log/
+[ ]$ mkdir -p /export/home/companydba/script/log/
 
 ------------------------------------------------------------------------------------------------
-[oracle@moschdb ~]$ cat /export/home/mohaymendba/script/locks_invdb4_find.sql
+[oracle@moschdb ~]$ cat /export/home/companydba/script/locks_db_find.sql
 set line 300
 col Blocking_session for a100
 col Blocked_session for a100
@@ -11,7 +11,7 @@ set feed off
 set head off
 alter session set nls_calendar=gregorian;
 alter session set nls_date_format='DD-MON-YY HH24:MI:SS';
-spool /export/home/mohaymendba/script/locks_invdb4.sql
+spool /export/home/companydba/script/locks_db.sql
 --set time on
 --set timing on
 select    '(Username='                  || sa.username
@@ -36,41 +36,38 @@ spool off
 exit;
 
 -----------------------------------------------------------------------------------------------
-[oracle@moschdb ~]$ cat /export/home/mohaymendba/script/locks_invdb4_alert.ksh
+[oracle@moschdb ~]$ cat /export/home/companydba/script/locks_db_alert.ksh
 
 #!/bin/bash
 
-export ORACLE_SID=invdb4
+export ORACLE_SID=db
 export ORACLE_HOME=/oracle/product/19c/db_1
 export LD_LIBRARY_PATH=/oracle/product/19c/db_1/lib:/lib
 export PATH=$ORACLE_HOME/bin:$PATH
 
 
 #Empty File Content by Redirecting to Null:
-> /export/home/mohaymendba/script/locks_invdb4.sql
+> /export/home/companydba/script/locks_db.sql
 
 #Run SQL Files:
-sqlplus INVENTIVE/InV19_Ts#400 << EOF
-@/export/home/mohaymendba/script/locks_invdb4_find.sql
+sqlplus USERNAME/PASSWORD << EOF
+@/export/home/companydba/script/locks_db_find.sql
 EOF
 
 #Send an Email if the file is not empty:
-if [[ -s /export/home/mohaymendba/script/locks_invdb4.sql ]]
+if [[ -s /export/home/companydba/script/locks_db.sql ]]
 then
-cat /export/home/mohaymendba/script/locks_invdb4.sql | mailx -s "INVDB4_ALERT_LOCKS" ext.h.esfandiari@mci.ir
-cat /export/home/mohaymendba/script/locks_invdb4.sql | mailx -s "INVDB4_ALERT_LOCKS" ext.m.chegenizadeh@mci.ir
-cat /export/home/mohaymendba/script/locks_invdb4.sql | mailx -s "INVDB4_ALERT_LOCKS" s.rostampour@mci.ir
-cat /export/home/mohaymendba/script/locks_invdb4.sql | mailx -s "INVDB4_ALERT_LOCKS" a.oliyaei@mci.ir
+cat /export/home/companydba/script/locks_db.sql | mailx -s "db_ALERT_LOCKS" ext.h.esfandiari@mci.ir
 fi
 
 #Empty File Content by Redirecting to Null:
-> /export/home/mohaymendba/script/locks_invdb4.sql
+> /export/home/companydba/script/locks_db.sql
 
 -----------------------------------------------------------------------------------------------
-[oracle@moschdb ~]$ chmod +x /export/home/mohaymendba/script/locks_invdb4_alert.ksh
+[oracle@moschdb ~]$ chmod +x /export/home/companydba/script/locks_db_alert.ksh
 
 [oracle@moschdb ~]$ crontab -e
-00,30 * * * * /export/home/mohaymendba/script/locks_invdb4_alert.ksh >> /export/home/mohaymendba/script/log/locks_invdb4_alert.log
+00,30 * * * * /export/home/companydba/script/locks_db_alert.ksh >> /export/home/companydba/script/log/locks_db_alert.log
 
 [oracle@moschdb ~]$ crontab -l
 
